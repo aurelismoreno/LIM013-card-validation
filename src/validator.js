3bd2636e232b7a3edcdd36e266c43902a84bb1e5
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-07-22 22:30:06 -0500

    funcion validar y enmascarar

--- src/validator.js
+++ src/validator.js
@@ -1,33 +1,36 @@
 const validator = {
-  isValid: function (creditCardNumber) {
-    // verificar antes que 'creditCardNumber', no tenga espacios
-   let numbers = creditCardNumber.split('');
-   let reverseNumbers = numbers.reverse();
-   let resultNumbers = [];
-    
-    for (let i = 0; i < numbers.length; i += 1) {
-        let positionArray = i + 1;
-        // Solo se multiplican los números en posiciones pares
-        if (positionArray % 2 === 0) {
-            let multiplication = reverseNumbers[i] * 2;
-            resultNumbers += multiplication;
-        } else {
-            /* Los números en posiciones impares se agregan
-            para posteriormente ser sumados entre con los
-            pares que se multiplicaron */
-               resultNumbers += reverseNumbers[i];
+    isValid: function (creditCardNumber) {
+        let numberArray = creditCardNumber.split('').reverse();
+        for (let i = 0; i < numberArray.length; i++) {
+            if (i % 2 != 0) {
+                numberArray[i] = numberArray[i] * 2;
+                if (numberArray[i] > 9) {
+                    numberArray[i] = parseInt(String(numberArray[i]).charAt(0)) + parseInt(String(numberArray[i]).charAt(1))
+                }
+            }
         }
+        let sum = 0;
+        for (let i = 1; i < numberArray.length; i++) {
+            sum += parseInt(numberArray[i]);
+        }
+        sum = sum * 9 % 10;
+        if (numberArray[0] == sum) {
+            return true
+        } else {
+            return false
+        }
+    },
+
+    maskify: function (creditCardNumber) {
+        /* if (creditCardNumber.length < 6) return creditCardNumber;
+        const last4Characters = creditCardNumber.substr(-4);
+        const firstCharacter = creditCardNumber.substr(0, 1);
+        const maskingCharacters = creditCardNumber.substr(1, creditCardNumber.length - 5).replace(/\d/g, '#');
+        return `${firstCharacter}${maskingCharacters}${last4Characters}`; */
+        return creditCardNumber.split('').map((letter, idx) => idx < creditCardNumber.length - 4 ? '#' : letter).join('');
+
     }
-    /* Total es un array con los números multiplicados
-    y los impares sin multiplicar, se alternan */
-    let total = resultNumbers.split('');
-    // Se suman todos los valores del array
-    total = total.reduce((a, b) => {
-        return parseInt(a) + parseInt(b);
-    });
-    return(true);
-    console.log(total);
-},
+
 
 };
 
