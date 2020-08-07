8102731fdef8bbae3020553e490ed8b391316805
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-08-06 22:08:54 -0500

    formulario datos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import validator from './validator.js';
+/* ------------------------PRINCIPAL */
+const btnregistrar = document.getElementById('btnregistrar');
 
-console.log(validator);
+btnregistrar.addEventListener('click', () => {
+	document.getElementById('principal').classList.add('ocultar');
+	document.getElementById('datospersonales').classList.remove('ocultar');
+});
+/* ----------------------DATOS   */
+const btndatos = document.getElementById('btndatos');
+
+btndatos.addEventListener('click', () => {
+	document.getElementById('datospersonales').classList.add('ocultar');
+	document.getElementById('formulario-tarjeta').classList.remove('ocultar');
+});
+
+/* ----------------------VALIDACION DE TARJETA  */
+const btnvalidar = document.getElementById('btnvalidar');
+const form = document.getElementById('formulario-tarjeta');
+const numero = document.getElementById('numero');
+
+
+btnvalidar.addEventListener('click', () => {
+	document.getElementById('formulario-tarjeta').classList.add('ocultar');
+	document.getElementById('gracias').classList.remove('ocultar');
+})
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+});
+
+function getUserInput() {
+	return numero.value; //Con input el tomo el valor que el usuario me da
+}
+
+numero.addEventListener('change', (e) => {
+	e.preventDefault();
+	if (validator.isValid(getUserInput())) {
+		alert('Tarjeta Valida');
+		console.log(validator.maskify(getUserInput()));
+	} else {
+		alert('Tarjeta Invalida');
+	}
+
+	if (validator.maskify(getUserInput())) {
+		return getUserInput();
+	}
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,36 @@
 const validator = {
 	isValid: function (creditCardNumber) {
+		//4817238647023382
+		//convirtiendo creditCardNumber en array con split e invirtiendolo con reverse
 		let numberArray = creditCardNumber.split('').reverse();
+
+		//recorriendo array invertido
 		for (let i = 0; i < numberArray.length; i++) {
+			//obtenido numeros de posiciones par
 			if (i % 2 != 0) {
 				numberArray[i] = numberArray[i] * 2;
 				if (numberArray[i] > 9) {
-					numberArray[i] =
-						parseInt(String(numberArray[i]).charAt(0)) + parseInt(String(numberArray[i]).charAt(1));
+					//sumando los digitos
+					// division entera https://www.it-swarm.dev/es/javascript/division-entera-con-resto-en-javascript/970450856/
+					numberArray[i] = (numberArray[i] % 10) + ((numberArray[i] / 10) >> 0);
+					/* parseInt(String(numberArray[i]).charAt(0)) + parseInt(String(numberArray[i]).charAt(1)); */
 				}
 			}
 		}
 		let sum = 0;
-		for (let i = 1; i < numberArray.length; i++) {
-			sum += parseInt(numberArray[i]);
+		//obteniendo numeros de posiciones impar
+		for (let i = 0; i < numberArray.length; i++) {
+			//sumando numeros posiciones par e impar
+			sum = sum + parseInt(numberArray[i]);
 		}
-		sum = (sum * 9) % 10;
+		//si el total de la suma modulo de 10 resulta 0 retorna true caso contarrio false
+		/* sum = (sum * 9) % 10;
 		if (numberArray[0] == sum) {
+			return true;
+		} else {
+			return false;
+		} */
+		if (sum % 10 == 0) {
 			return true;
 		} else {
 			return false;
@@ -23,7 +38,19 @@ const validator = {
 	},
 
 	maskify: function (creditCardNumber) {
-		let allButLastFour = creditCardNumber.slice(0, creditCardNumber.length - 4);
+		let newString = '';
+		for (let i = 0; i < creditCardNumber.length; i++) {
+			let offset = creditCardNumber.length - 4;
+			// console.log(offset);
+			if (i < offset) {
+				newString += '#';
+			} else {
+				newString += creditCardNumber[i];
+			}
+		}
+		return newString;
+
+		/* let allButLastFour = creditCardNumber.slice(0, creditCardNumber.length - 4);
 		let hashHole = [];
 		let lastFour = creditCardNumber.slice(-4);
 
@@ -36,7 +63,7 @@ const validator = {
 		}
 		var conjoinedHashHole = hashHole.join('');
 
-		return conjoinedHashHole + lastFour;
+		return conjoinedHashHole + lastFour; */
 	},
 };
 
