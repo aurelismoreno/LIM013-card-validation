e5df627894da3e8d70eaf27d6bc395d5c462cbbc
Aurelis Moreno <aurelis.moreno@gmail.com>
2020-08-07 10:57:55 -0500

    imagenes prototipo

--- src/index.js
+++ src/index.js
@@ -1,49 +1,45 @@
 import validator from './validator.js';
-/* ------------------------PRINCIPAL */
+/* ------------------------PRINCIPAL  */
 const btnregistrar = document.getElementById('btnregistrar');
 
 btnregistrar.addEventListener('click', () => {
 	document.getElementById('principal').classList.add('ocultar');
 	document.getElementById('datospersonales').classList.remove('ocultar');
 });
-/* ----------------------DATOS   */
-const btndatos = document.getElementById('btndatos');
+/* ----------------------DATOS */   
+const btncontinuar = document.getElementById('btncontinuar');
 
-btndatos.addEventListener('click', () => {
+btncontinuar.addEventListener('click', () => {
 	document.getElementById('datospersonales').classList.add('ocultar');
 	document.getElementById('formulario-tarjeta').classList.remove('ocultar');
 });
 
-/* ----------------------VALIDACION DE TARJETA  */
+/* ----------------------VALIDACION DE TARJETA */  
 const btnvalidar = document.getElementById('btnvalidar');
 const form = document.getElementById('formulario-tarjeta');
-const numero = document.getElementById('numero');
 
-
+const numero = document.getElementById('numero'); 
 btnvalidar.addEventListener('click', () => {
 	document.getElementById('formulario-tarjeta').classList.add('ocultar');
 	document.getElementById('gracias').classList.remove('ocultar');
-})
+});
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 });
 
 function getUserInput() {
-	return numero.value; //Con input el tomo el valor que el usuario me da
+	return numero.value;
 }
 
 numero.addEventListener('change', (e) => {
 	e.preventDefault();
 	if (validator.isValid(getUserInput())) {
 		alert('Tarjeta Valida');
-		console.log(validator.maskify(getUserInput()));
 	} else {
 		alert('Tarjeta Invalida');
 	}
-
 	if (validator.maskify(getUserInput())) {
-		return getUserInput();
+		return numero;
 	}
 });
-
